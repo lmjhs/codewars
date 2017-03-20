a6e3dac39502735f070d42ace0ef00905728b6de
Luke Spencer <luke.spencer@lexisnexis.com>
2017-03-20 09:08:18 +0000

    Completed all tests

--- 4-by-4-skyscrapers/solve-puzzle.js
+++ 4-by-4-skyscrapers/solve-puzzle.js
@@ -21,7 +21,7 @@ exports.solvePuzzle = function solvePuzzle(clues) {
     this.solve = function() {
       while (that.options.length > 0) {
         that.result = this.options.shift();
-        console.log(grid.getResult());
+        //console.log(grid.getResult());
         // console.log('Result: ' + that.result);
         if (!that.row.hasDuplicates() && !that.column.hasDuplicates()) {
           return true;
@@ -32,14 +32,17 @@ exports.solvePuzzle = function solvePuzzle(clues) {
     }
   }
 
-  function row() {
+  function row(index) {
+    this.index = index;
     this.leftClue = 0;
     this.rightClue = 0; 
     var that = this;
     this.cells = [];
+
     this.addCell = function(cell) {
       this.cells.push(cell);
     };
+
     this.getResult = function() {
       var result = [];
       that.cells.forEach(function(cell) {
@@ -52,10 +55,23 @@ exports.solvePuzzle = function solvePuzzle(clues) {
     this.solve = function() {
       while (cell >= 0 && cell < that.cells.length) {
         if (that.cells[cell].solve()) {
-          cell++;
+          // We are in the last row so check the column is valid
+          if (that.index == that.cells.length) {
+            if (grid.columns[cell].valid()) {
+              cell++;
+            } else {
+              that.cells[cell].reset();
+              cell--;
+            }
+          } else {
+            cell++;
+          }
         } else {
           cell--;
         }
+        
+
+        // Its the end of the row so check that the row is valid
         if (cell == that.cells.length) {
           if (!that.valid()) {
             cell--;
@@ -71,6 +87,7 @@ exports.solvePuzzle = function solvePuzzle(clues) {
       cell = 0;
       return false;
     }
+
     this.hasDuplicates = function() {
       var seen = [];
       var valid = false;
@@ -85,9 +102,11 @@ exports.solvePuzzle = function solvePuzzle(clues) {
 
       return valid;
     }
+
     this.valid = function() {
       return that.leftClueValid() && that.rightClueValid();
     }
+
     this.leftClueValid = function() {
       if (that.leftClue > 0) {
         var highest = 0;
@@ -98,7 +117,7 @@ exports.solvePuzzle = function solvePuzzle(clues) {
             count++;
           }
         });
-        console.log('Left Clue Count: ' + count + ' / ' + that.leftClue);
+        // console.log('Left Clue Count: ' + count + ' / ' + that.leftClue);
         if (count == that.leftClue){
           return true;
         }
@@ -117,7 +136,7 @@ exports.solvePuzzle = function solvePuzzle(clues) {
             count++;
           }
         };
-        console.log('Right Clue Count: ' + count + ' / ' + that.rightClue);
+        // console.log('Right Clue Count: ' + count + ' / ' + that.rightClue);
         if (count == that.rightClue){
           return true;
         }
@@ -127,14 +146,17 @@ exports.solvePuzzle = function solvePuzzle(clues) {
     }
   }
 
-  function column() {
+  function column(index) {
     var that = this;
+    this.index = index;
     this.topClue = 0;
     this.bottomClue = 0; 
     this.cells = [];
+
     this.addCell = function(cell) {
       this.cells.push(cell);
     }
+
     this.hasDuplicates = function() {
       var seen = [];
       var valid = false;
@@ -149,16 +171,59 @@ exports.solvePuzzle = function solvePuzzle(clues) {
 
       return valid;
     }
+
+    this.valid = function() {
+      return that.topClueValid() && that.bottomClueValid();
+    }
+
+    this.topClueValid = function() {
+      if (that.topClue > 0) {
+        var highest = 0;
+        var count = 0;
+        that.cells.forEach(function(cell) {
+          if (cell.result > highest) {
+            highest = cell.result;
+            count++;
+          }
+        });
+        // console.log('Top Clue Count: ' + count + ' / ' + that.topClue);
+        if (count == that.topClue){
+          return true;
+        }
+        return false;
+      }
+      return true;
+    }
+
+    this.bottomClueValid = function() {
+      if (that.bottomClue > 0) {
+        var highest = 0;
+        var count = 0;
+        for (i = that.cells.length - 1; i >= 0; i--) {
+          if (that.cells[i].result > highest) {
+            highest = that.cells[i].result;
+            count++;
+          }
+        };
+        // console.log('Bottom Clue Count: ' + count + ' / ' + that.bottomClue);
+        if (count == that.bottomClue){
+          return true;
+        }
+        return false;
+      }
+      return true;
+    }
   }
 
   function grid(size, clues) {
     var that = this;
     this.columns = [];
     this.rows = [];
+    this.size = size;
 
     for (i = 1; i <= size; i++) {
-      this.columns.push(new column());
-      this.rows.push(new row());
+      this.columns.push(new column(i));
+      this.rows.push(new row(i));
     }
 
     that.rows.forEach(function(row, rowIndex) {
@@ -170,14 +235,14 @@ exports.solvePuzzle = function solvePuzzle(clues) {
     });
 
     clues.forEach(function(clue, index) {
-      if (index < 4) {
+      if (index < that.size) {
         that.columns[index].topClue = clue;
-      } else if (index < 8) {
-        that.rows[index - 4].rightClue = clue;
-      } else if (index < 12) {
-        that.columns[11 - index].bottomClue = clue;
+      } else if (index < (that.size * 2)) {
+        that.rows[index - that.size].rightClue = clue;
+      } else if (index < (that.size * 3)) {
+        that.columns[(that.size * 3) - 1 - index].bottomClue = clue;
       } else {
-        that.rows[15 - index].leftClue = clue;
+        that.rows[(that.size * 4) - 1 - index].leftClue = clue;
       }
     });
 
@@ -186,10 +251,10 @@ exports.solvePuzzle = function solvePuzzle(clues) {
       var row = 0
       while (row >= 0 && row < that.rows.length) {
         if (that.rows[row].solve()) {
-          console.log('Solved Row');
+          // console.log('Solved Row');
           row++;
         } else {
-          console.log('Failed Row');
+          // console.log('Failed Row');
           row--;
         }
       }
@@ -206,10 +271,5 @@ exports.solvePuzzle = function solvePuzzle(clues) {
   }
 
   var grid = new grid(4, clues);
-  grid.solve();
-
-  console.log(clues);
-  console.log(grid.getResult());
-
-  return grid.getResult();
+  return grid.solve();
 }
